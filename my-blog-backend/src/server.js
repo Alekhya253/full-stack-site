dea84fce05a1d6aebc0cd6980e683042dbe7ae9b
Alekhya253 <20wh1a05b6@bvrithyderabad.edu.in>
2023-05-25 17:52:41 +0530

    added comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'learn-mongodb',
     upvotes: 0,
+    comments: [],
   },
 ];
 const app = express();
@@ -32,6 +35,19 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     res.send(`The ${name} article has ${article.upvotes} upvotes !!`);
   } else {
     res.send(`article ${name} doesnot exist`);
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send('doesnot exist');
   }
 });
 app.listen(8000, () => {
